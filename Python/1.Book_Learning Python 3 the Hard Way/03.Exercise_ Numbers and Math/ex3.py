c50ad256535fd7e472c3ee0b3a8d23daf2d54632
ldev5 <dhayes11@proton.me>
2023-05-11 05:46:28 -0500

    new file:   Python/1.Book_Learning Python 3 the Hard Way/03.Exercise_ Numbers and Math/ex3.py

---
+++ Python/1.Book_Learning Python 3 the Hard Way/03.Exercise_ Numbers and Math/ex3.py
@@ -0,0 +1,24 @@
+#Prints a string
+print("I will now count my chickens:")
+
+#Prints a string and its corresponding value based on the result of arithmetic 
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+#Prints a string
+print("Now I will count the eggs:")
+
+#Prints the result of some arithmetic
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+#Prints a string
+print("Is it true that 3 + 2 < 5 - 7?")
+#Prints the result of some arithmetic related to the question posed by the previous string
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+#Prints a string and the result of some arithmetic which demonstrate the logic of the output on line 17
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+#Prints a string
+print("Oh, that's why it's False")
